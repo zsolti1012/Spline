063b72c50896e2d2a5b7ac3317c648959dfd9f54
zsolti1012 <56861547+zsolti1012@users.noreply.github.com>
2021-05-19 16:25:55 +0200

    Upload

---
+++ __AlapRajzolo/Form1.Designer.cs
@@ -0,0 +1,184 @@
+﻿namespace __AlapRajzolo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.canvas = new System.Windows.Forms.PictureBox();
+            this.button_delete = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.canvas)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // canvas
+            // 
+            this.canvas.BackColor = System.Drawing.Color.YellowGreen;
+            this.canvas.Location = new System.Drawing.Point(12, 12);
+            this.canvas.Name = "canvas";
+            this.canvas.Size = new System.Drawing.Size(760, 437);
+            this.canvas.TabIndex = 0;
+            this.canvas.TabStop = false;
+            this.canvas.Paint += new System.Windows.Forms.PaintEventHandler(this.canvas_Paint);
+            this.canvas.DoubleClick += new System.EventHandler(this.canvas_DoubleClick);
+            this.canvas.MouseDown += new System.Windows.Forms.MouseEventHandler(this.canvas_MouseDown);
+            this.canvas.MouseMove += new System.Windows.Forms.MouseEventHandler(this.canvas_MouseMove);
+            this.canvas.MouseUp += new System.Windows.Forms.MouseEventHandler(this.canvas_MouseUp);
+            this.canvas.MouseWheel += new System.Windows.Forms.MouseEventHandler(this.canvas_MouseWheel);
+            // 
+            // button_delete
+            // 
+            this.button_delete.Location = new System.Drawing.Point(56, 39);
+            this.button_delete.Name = "button_delete";
+            this.button_delete.Size = new System.Drawing.Size(75, 23);
+            this.button_delete.TabIndex = 1;
+            this.button_delete.Text = "Töröl";
+            this.button_delete.UseVisualStyleBackColor = true;
+            this.button_delete.Click += new System.EventHandler(this.button_delete_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.label6);
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(13, 450);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(759, 80);
+            this.panel1.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.SystemColors.Control;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.label3.Location = new System.Drawing.Point(222, 17);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(333, 20);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "A sárga pöttyel a szakasz mozgathatóvá válik.";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.Control;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.Location = new System.Drawing.Point(190, -3);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(398, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "A fekete négyzetekre kattintva mozgathatni tudjuk őket";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.SystemColors.Control;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label2.ForeColor = System.Drawing.Color.Black;
+            this.label2.Location = new System.Drawing.Point(14, 37);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(733, 20);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "A           és             pötty esetén, bal klikkel forgatható a szakasz, jobb e" +
+    "gérgombbal a pont csúsztatható \r\n";
+            this.label2.Click += new System.EventHandler(this.label2_Click_1);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.SystemColors.Control;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label4.Location = new System.Drawing.Point(309, 58);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(152, 20);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "az egyenes mentén.";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.SystemColors.Control;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label5.ForeColor = System.Drawing.Color.Blue;
+            this.label5.Location = new System.Drawing.Point(37, 37);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(34, 20);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "kék";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.SystemColors.Control;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label6.ForeColor = System.Drawing.Color.Red;
+            this.label6.Location = new System.Drawing.Point(93, 37);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(47, 20);
+            this.label6.TabIndex = 6;
+            this.label6.Text = "vörös\r\n";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 534);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.button_delete);
+            this.Controls.Add(this.canvas);
+            this.Name = "Form1";
+            this.Text = "Spline3 - Oravecz Zsolt";
+            ((System.ComponentModel.ISupportInitialize)(this.canvas)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox canvas;
+        private System.Windows.Forms.Button button_delete;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ __AlapRajzolo/Form1.cs
@@ -0,0 +1,343 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace __AlapRajzolo
+{
+    public partial class Form1 : Form
+    {
+        Graphics g;
+        Color colorControl = Color.Black;
+        Color colorCurve = Color.Blue;
+        Color colorCurve2 = Color.Red;
+        List<PointF> P = new List<PointF>();
+        int grab = -1;
+        float width;
+        float heigth;
+        float dx=0, dy=0;
+        bool gotcha1 = false;
+        bool gotcha2 = false;
+
+        bool doubleCLick = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+            
+            width = canvas.Width;
+            heigth = canvas.Height;
+            
+            
+        }
+
+        private void canvas_Paint(object sender, PaintEventArgs e)
+        {
+            g = e.Graphics;
+            
+            if (gotcha1)
+            {
+                g.DrawLine(new Pen(Color.Red,3f), P[grab - 1], Add(Subs(P[grab - 2], P[grab - 1]), P[grab - 1]));
+            }
+            if (gotcha2)
+            {
+                g.DrawLine(new Pen(Color.Red, 3f), P[grab +1], Add(Subs(P[grab + 2], P[grab + 1]), P[grab + 1]));
+            }
+            for (int i = 0; i < P.Count-3; i+=3)
+            {
+                DrawBezier3Arc(new Pen(colorCurve,3f),P[i],P[i+1],P[i+2],P[i+3]);
+            }
+
+            for (int i = 0; i < P.Count; i++)
+            {
+                
+                if  (i > 1 && (i) % 3 == 0)
+                    g.FillEllipse(new SolidBrush(Color.Yellow), P[i].X - 5, P[i].Y - 5, 10, 10);
+                else if (i > 1 && (i+1) % 3 == 0)
+                    g.FillEllipse(new SolidBrush(Color.LightBlue), P[i].X - 5, P[i].Y - 5, 10, 10);
+                else if(i>1 &&(i-1)%3==0)
+                    g.FillEllipse(new SolidBrush(Color.Red), P[i].X - 5, P[i].Y - 5, 10, 10);
+                
+                else g.FillRectangle(new SolidBrush(colorControl),P[i].X-5,P[i].Y-5,10,10);
+            }
+
+            for (int i = 0; i < P.Count-1; i++)
+            {
+                g.DrawLine(new Pen(colorControl),P[i],P[i+1]);
+            }
+        }
+
+        #region Mouse Handling
+        private void canvas_MouseDown(object sender, MouseEventArgs e)
+        {
+            
+            for (int i = 0; i < P.Count; i++)
+            {
+                if (IsGrab(P[i],e.Location))
+                {
+                    grab = i;
+                    
+                }
+            }
+            if (grab==-1)
+            {
+                PointF point;
+                PointF location = e.Location;
+                
+                
+
+                //A harmadik pont megfogása
+                 if (P.Count >1 && (P.Count-1) % 3 == 0 && P.Count != 0)
+                {
+                    //
+                    
+                    float m = (P[P.Count - 1].Y - P[P.Count - 2].Y) / (P[P.Count - 1].X - P[P.Count - 2].X);
+                    float b = P[P.Count - 1].Y - m * P[P.Count - 1].X;
+
+                    if (P[P.Count - 1].X < P[P.Count - 2].X && (location.Y - b) / m < 0)
+                        point = new PointF(0, (location.Y));
+                    else if (P[P.Count - 1].X > P[P.Count - 2].X && (location.Y - b) / m > width)
+                        point = new PointF(width, (location.Y));
+
+                    else {
+                        if (P[P.Count - 2].Y > P[P.Count - 1].Y && location.Y > P[P.Count - 1].Y)
+                        {
+                            point = new PointF((P[P.Count-1].Y-20 - b) / m, (P[P.Count - 1].Y - 20));
+                        }
+                        else if (P[P.Count - 2].Y < P[P.Count - 1].Y && location.Y < P[P.Count - 1].Y)
+                            point = new PointF((P[P.Count - 1].Y + 20 - b) / m, (P[P.Count - 1].Y + 20));
+
+                        else point = new PointF((location.Y - b) / m, (location.Y));
+                    }
+                    P.Add(point);
+                }
+                    else
+                    {
+                        P.Add(e.Location);
+                    }
+
+               
+                
+               
+
+                
+                grab = P.Count - 1;
+                canvas.Refresh();
+                
+            }
+        }
+        private void canvas_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (grab!=-1)
+            {
+                if (grab != P.Count - 1 && grab >= 3 && grab != 1 && (grab) % 3 == 0)
+                {
+                    //A köztes pont megfogása
+                    dx = e.X - P[grab].X;
+                    dy = e.Y - P[grab].Y;
+
+                    P[grab] = e.Location;
+
+                    PointF tmp = new PointF(P[grab + 1].X + (dx), P[grab + 1].Y + (dy));
+
+                    P[grab + 1] = tmp;
+                    tmp = new PointF(P[grab - 1].X + (dx), P[grab - 1].Y + (dy));
+                    P[grab - 1] = tmp;
+                }
+
+                //Az i-edik görbe harmadik pontja 
+                else if ( grab <= P.Count - 3 && (grab + 1) % 3 == 0)
+                {
+                   
+
+                    if (e.Button == MouseButtons.Right)
+                    {
+                        //A segédszakasz kirajzolásához egy változó
+                        gotcha2 = true;
+
+                        //Az i+1-edik görbe második pontja
+                        float m = (P[grab + 1].Y - P[grab + 2].Y) / (P[grab + 1].X - P[grab + 2].X);
+                        float b = P[grab + 1].Y - m * P[grab + 1].X;
+
+                        //Elkerüljük a pontnak való nekiütközést
+                        if ((P[grab + 1].Y > P[grab + 2].Y && e.Location.Y > P[grab + 1].Y + 5)||
+                            (P[grab+1].Y<P[grab+2].Y&&e.Location.Y<P[grab+1].Y-5))
+                        {
+                            PointF point = new PointF((e.Y - b) / m, (e.Y));
+                            P[grab] = point;
+                        }
+                    }
+                    if (e.Button == MouseButtons.Left)
+                    {
+                        
+                        //Arány kiszámítása
+                        float arany = Math.Abs((float)Math.Sqrt(Math.Pow(P[grab + 1].X - P[grab + 2].X, 2) + Math.Pow(P[grab + 1].Y - P[grab + 2].Y, 2)) /
+                            (float)Math.Sqrt(Math.Pow(P[grab].X - P[grab + 1].X, 2) + Math.Pow(P[grab].Y - P[grab + 1].Y, 2)));
+                        ;
+
+                        dx = P[grab].X - P[grab + 1].X;
+                        dy = P[grab].Y - P[grab + 1].Y;
+
+
+                        //Az aránnyal meg kell szoroznunk a távolságot, hiszen nem ugyan azon a köríven mozognak
+
+                        PointF tmp = new PointF(P[grab + 1].X - (dx * arany), P[grab + 1].Y - (dy * arany));
+                        P[grab] = e.Location;
+
+
+                        P[grab + 2] = tmp;
+                    }
+
+
+                }
+
+                //Az i+1 görbe második pontja 
+                else if (  grab !=1&&(grab - 1) % 3 == 0)
+                {
+                    if (e.Button == MouseButtons.Right) {
+                        //A segédszakasz kirajzolásához egy változó
+                        gotcha1 = true;
+
+                        //Az i+1-edik görbe második pontja
+                        float m = (P[grab - 1].Y - P[grab - 2].Y) / (P[grab - 1].X - P[grab - 2].X);
+                        float b = P[grab - 1].Y - m * P[grab - 1].X;
+                        if ((P[grab - 1].Y > P[grab - 2].Y && e.Location.Y > P[grab - 1].Y + 5) ||
+                            (P[grab - 1].Y < P[grab - 2].Y && e.Location.Y < P[grab - 1].Y - 5))
+                        {
+                            PointF point = new PointF((e.Y - b) / m, (e.Y));
+                            P[grab] = point;
+                        }
+                    }
+                    if (e.Button == MouseButtons.Left)
+                    {
+                        float arany = Math.Abs((float)Math.Sqrt(Math.Pow(P[grab - 1].X - P[grab - 2].X, 2) + Math.Pow(P[grab - 1].Y - P[grab - 2].Y, 2)) /
+                        (float)Math.Sqrt(Math.Pow(P[grab].X - P[grab - 1].X, 2) + Math.Pow(P[grab].Y - P[grab - 1].Y, 2)));
+                        ;
+
+                        dx = P[grab].X - P[grab - 1].X;
+                        dy = P[grab].Y - P[grab - 1].Y;
+
+
+                        //Az aránnyal meg kell szoroznunk a távolságot, hiszen nem ugyan azon a köríven mozognak
+                        float m = (P[P.Count - 1].Y - P[P.Count - 2].Y) / (P[P.Count - 1].X - P[P.Count - 2].X);
+                        float b = P[P.Count - 1].Y - m * P[P.Count - 1].X;
+                        PointF tmp = new PointF(P[grab - 1].X - (dx * arany), P[grab - 1].Y - (dy * arany));
+
+                        P[grab] = e.Location;
+                        P[grab - 2] = tmp;
+                    }
+                    
+                }
+                
+                
+                else
+                    P[grab] = e.Location;
+
+                canvas.Refresh();
+            }
+        }
+        private void canvas_MouseUp(object sender, MouseEventArgs e)
+        {
+            grab = -1;
+            gotcha1 = false;
+            gotcha2 = false;
+            doubleCLick = false;
+        }
+        private void canvas_MouseWheel(object sender, MouseEventArgs e)
+        {
+
+        }
+        #endregion
+
+        private bool IsGrab(PointF p, PointF mouseLocation)
+        {
+            return p.X - 5 <= mouseLocation.X && mouseLocation.X <= p.X + 5 &&
+                   p.Y - 5 <= mouseLocation.Y && mouseLocation.Y <= p.Y + 5;
+        }
+
+        private float Binom(int n,int k)
+        {
+            if (k == 0) return 1;
+            if (k == n) return 1;
+            if (n == 0) return 0;
+            return Binom(n-1,k-1)+Binom(n-1,k);
+        }
+
+
+
+        private double B0(double t) { return (1 - t) * (1 - t) * (1 - t); }
+        private double B1(double t) { return 3 * t * (1 - t) * (1 - t); }
+        private double B2(double t) { return 3 * t * t * (1 - t); }
+        private double B3(double t) { return t * t * t; }
+
+        private void DrawBezier3Arc(Pen pen,
+            PointF p0, PointF p1, PointF p2, PointF p3)
+        {
+            double a = 0;
+            double t = a;
+            double h = 1.0 / 500.0;
+            PointF d0, d1;
+            d0 = new PointF((float)(B0(t) * p0.X + B1(t) * p1.X + B2(t) * p2.X + B3(t) * p3.X),
+                            (float)(B0(t) * p0.Y + B1(t) * p1.Y + B2(t) * p2.Y + B3(t) * p3.Y));
+            while (t < 1)
+            {
+                t += h;
+                d1 = new PointF((float)(B0(t) * p0.X + B1(t) * p1.X + B2(t) * p2.X + B3(t) * p3.X),
+                                (float)(B0(t) * p0.Y + B1(t) * p1.Y + B2(t) * p2.Y + B3(t) * p3.Y));
+                g.DrawLine(pen, d0, d1);
+                d0 = d1;
+            }
+        }
+
+        private PointF Add(PointF a, PointF b)
+        {
+            return new PointF(b.X + a.X, b.Y + a.Y);
+        }
+        private PointF Subs(PointF a, PointF b)
+        {
+            return new PointF(b.X - a.X, b.Y - a.Y);
+        }
+
+        private void button_delete_Click(object sender, EventArgs e)
+        {
+            P = new List<PointF>();
+            dx = 0;
+            dy = 0;
+            grab = -1;
+            gotcha1 = false;
+            gotcha2 = false;
+            canvas.Refresh();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void canvas_DoubleClick(object sender, EventArgs e)
+        {
+            doubleCLick = true;
+        }
+
+        private void label2_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private PointF Mult(PointF a, float l)
+        {
+            return new PointF(a.X * l, a.Y * l);
+        }
+
+
+
+
+
+
+    }
+}
